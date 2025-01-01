b3c7ac8c41ef40d9e75de6b48f2dd81ed328541a
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2025-01-01 21:01:39 +0600

    first commit

---
+++ src/main/java/com/example/electronicsshop/ElectronicsShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.electronicsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicsshopApplication.class, args);
+    }
+
+}
